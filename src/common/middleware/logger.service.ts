00092103e95f8b2e22158876b0765ee54d09a976
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2024-11-09 05:33:31 -0400

    feat: 💫 add module logger

---
+++ src/common/middleware/logger.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import pino from 'pino';
+
+@Injectable()
+export class LoggerService {
+  private readonly loggerInstance = pino({
+    level: 'info',
+  });
+
+  get logger(): pino.Logger {
+    return this.loggerInstance;
+  }
+
+  logInfo(message: string, additionalData?: any): void {
+    this.logger.info({ message, additionalData });
+  }
+
+  logDebug(message: string, additionalData?: any): void {
+    this.logger.debug({ message, additionalData });
+  }
+
+  logError(message: string, additionalData?: any): void {
+    this.logger.error({ message, additionalData });
+  }
+}
